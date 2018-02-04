0e1974d60b7fadf624122bc70f4bfdbe3f0d2d5e
Simon Schubert <2@0x2c.org>
2018-02-04 13:11:55 +0100

    add constant power mode

--- m98.py
+++ m98.py
@@ -97,6 +97,11 @@ class M98:
         self._cmd(Cmd.Battery_Test)
         return (self.voltage, self.current, self.capacity)
 
+    def cw_mode(self, power):
+        self.conn.write_registers(Reg.PFIX, _float(power), skip_encode=True, unit=1)
+        self._cmd(Cmd.CW)
+        return (self.voltage, self.current)
+
     def voltage(self):
         return _defloat(self.conn.read_holding_registers(Reg.U, 2, unit=1).registers)[0]
 
@@ -118,22 +123,24 @@ class Command:
             args.out = open(os.devnull, 'w')
             args.verbose = True
 
+        if not args.condition:
+            args.condition = lambda m, args: m.enabled()
+
         with m.enable(True):
             csvout = csv.writer(args.out)
             csvout.writerow(l.names)
-            for data in l.run(interval=args.interval, condition=m.enabled):
+            for data in l.run(interval=args.interval, condition=lambda: args.condition(m, args)):
                 if args.verbose:
                     print(l.pretty_record(data))
                 csvout.writerow(data)
                 args.out.flush()
 
-
     @classmethod
-    def _generic_parser(cls, p, mode):
+    def _generic_parser(cls, p, mode, condition=None):
         p.add_argument('--interval', type=float, default=1.0)
         p.add_argument('--out', '-o', type=argparse.FileType('w'))
         p.add_argument('--verbose', action='store_const', const=True, default=False)
-        p.set_defaults(func=cls._run, mode=mode)
+        p.set_defaults(func=cls._run, mode=mode, condition=condition)
 
     @classmethod
     def add_parsers(cls, parser):
@@ -149,6 +156,12 @@ class Command:
         ccp.add_argument('--current', '-i', type=float, required=True)
         ccp.add_argument('--risetime', type=float, default=None)
         cls._generic_parser(ccp, lambda m, args: m.cc_mode(current=args.current, risetime=args.risetime))
+
+        cwp = subp.add_parser('cw', aliases=['constant-power'], help='Constant power test')
+        cwp.add_argument('--power', type=float, required=True)
+        cwp.add_argument('--end-voltage', '-v', type=float, required=False)
+        cls._generic_parser(cwp, lambda m, args: m.cw_mode(power=args.power),
+                            condition=lambda m, args: m.enabled() and (not args.end_voltage or m.voltage() > args.end_voltage))
 
 
 if __name__ == '__main__':
